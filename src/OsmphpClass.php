8aca12387c397c3ea0ba40ce53127fbd9385065a
Alessio Piccioli <piccioli@netseven.it>
2023-08-22 15:45:51 +0200

    Initial version after configuration

---
+++ src/OsmphpClass.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Webmapp\Osmphp;
+
+class OsmphpClass
+{
+}
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Webmapp\Osmphp\Tests;
 
 use PHPUnit\Framework\TestCase;
 
